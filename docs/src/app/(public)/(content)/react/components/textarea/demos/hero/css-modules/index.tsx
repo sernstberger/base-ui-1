59e3050f6b034731e6951b34b5736020e9395d5c
Steve Ernstberger <steve32285@gmail.com>
2025-02-04 09:03:25 -0500

    small improvements

--- docs/src/app/(public)/(content)/react/components/textarea/demos/hero/css-modules/index.tsx
+++ docs/src/app/(public)/(content)/react/components/textarea/demos/hero/css-modules/index.tsx
@@ -3,5 +3,7 @@ import { Textarea } from '@base-ui-components/react/textarea';
 import styles from './index.module.css';
 
 export default function ExampleTextarea() {
-  return <Textarea placeholder="Name" className={styles.Textarea} rows={4} />;
+  return (
+    <Textarea placeholder="Some long text..." className={styles.Textarea} rows={4} />
+  );
 }
--- docs/src/app/(public)/(content)/react/components/textarea/demos/hero/tailwind/index.tsx
+++ docs/src/app/(public)/(content)/react/components/textarea/demos/hero/tailwind/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { Textarea } from '@base-ui-components/react/textarea';
 
-export default function ExampleInput() {
+export default function ExampleTextarea() {
   return (
     <Textarea
-      placeholder="Name"
-      className="h-10 w-full max-w-64 rounded-md border border-gray-200 pl-3.5 text-base text-gray-900 focus:outline focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
+      placeholder="Some long text..."
+      className="h-auto w-full max-w-64 rounded-md border border-gray-200 pl-3.5 text-base text-gray-900 focus:outline focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
       rows={4}
     />
   );
